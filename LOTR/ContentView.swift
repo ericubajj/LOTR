00e0ec9aaa9eae3b20367c795b1a1b3a91b54f36
ericubajj <ercubii123gmail.com>
2024-12-27 22:03:45 +0100

    Initial Commit

---
+++ LOTR/ContentView.swift
@@ -0,0 +1,114 @@
+//
+//  ContentView.swift
+//  LOTR
+//
+//  Created by Eri on 25.12.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var showExchangeInfo = false
+    @State var leftAmount = ""
+    @State var rightAmount = ""
+    
+    var body: some View {
+        ZStack {
+            //Background Image
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                //Prancing Pony Image
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                
+                //Currency Exchange Text
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                
+                //Conversion section
+                HStack {
+                    // Left Conversion Section
+                    VStack {
+                        // Currency
+                        HStack {
+                            // Currency Image
+                            Image(.silverpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                            
+                            // Currency Text
+                            Text("Silver Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                        }
+                        
+                        .padding(.bottom, -5)
+                        // Text Field
+                        TextField("Amount", text: $leftAmount)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    
+                    // Equal Sign
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .symbolEffect(.pulse)
+                    
+                    // Right Conversion Section
+                    VStack {
+                        // Currency
+                        HStack {
+                            // Currency Text
+                            Text("Gold Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                            // Currency Image
+                            Image(.goldpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                        }
+                        .padding(.bottom, -5)
+                        // Text Field
+                        TextField("Amount", text: $rightAmount)
+                            .textFieldStyle(.roundedBorder)
+                            .multilineTextAlignment(.trailing)
+                    }
+                }
+                .padding()
+                .background(.black.opacity(0.5))
+                .clipShape(.capsule)
+                
+                Spacer()
+                
+                // Info Button
+                HStack {
+                    Spacer()
+                    
+                    Button {
+                        showExchangeInfo.toggle()
+                    } label: {
+                        Image(systemName: "info.circle.fill")
+                            .font(.largeTitle)
+                            .foregroundStyle(.white)
+                    }
+                    .padding(.trailing)
+                }
+                
+                
+            }
+//             .border(.blue)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LOTR/LOTRApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRApp.swift
+//  LOTR
+//
+//  Created by Eri on 25.12.24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
