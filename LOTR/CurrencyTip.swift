9d418bf68a2387016053141d30738c3660660bd3
Eridon Cubaj <104393366+ericubajj@users.noreply.github.com>
2025-01-11 02:30:28 +0100

    Update CurrencyTip.swift

--- LOTR/CurrencyTip.swift
+++ LOTR/CurrencyTip.swift
@@ -8,7 +8,12 @@
 import TipKit
 
 struct CurrencyTip: Tip {
-    var title: Text
+    var title = Text("Change Currency")
+    
+    var message: Text? = Text("You can tap the left or right currency to bring up the Select Currency screen.")
+    
+    var image: Image? = Image(systemName: "hand.tap.fill")
     
     
 }
+
